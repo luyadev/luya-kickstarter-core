cac92176c0c0531dcc25f63d3810a2e87035212e
Nadar <git@nadar.io>
2016-10-16 14:24:26 +0200

    core application kickstarter init

---
+++ views/layouts/main.php
@@ -0,0 +1,20 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $content string */
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody(); ?>
+    <?= $content ?>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
