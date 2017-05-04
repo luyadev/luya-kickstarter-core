b297b59182f9b10132aacfaeba3183ad19ecd6ef
Basil Suter <git@nadar.io>
2017-05-04 11:26:53 +0200

    use luya config best practice

--- configs/env-local.php
+++ configs/env-local.php
@@ -1,1 +1,37 @@
 <?php
+
+define('YII_DEBUG', true);
+define('YII_ENV', 'local');
+
+$config = [
+    'id' => 'myapp',
+    'siteTitle' => 'My Application',
+    'luyaLanguage' => 'en',
+    'basePath' => dirname(__DIR__),
+    'components' => [
+        'mail' => [
+            'password' => '*********',
+        ],
+        'composition' => [
+            'default' => [
+                'langShortCode' => 'en'
+            ],
+            'hidden' => true,
+        ],
+        'urlManager' => [
+            'rules' => [
+                'home' => 'site/default/index',
+                'contact' => 'site/default/contact',
+            ],
+        ],
+    ],
+];
+
+if (YII_DEBUG) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return \yii\helpers\ArrayHelper::merge($config, require('env-local-db.php'));
